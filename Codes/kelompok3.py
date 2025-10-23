193ee37acd4a1b1e58b337cd7ba1dd7e57d26891
Muhammad Andria Almadani <c030324028@mahasiswa.poliban.ac.id>
2025-10-23 18:29:04 +0800

    Add files via upload

---
+++ Codes/kelompok3.py
@@ -0,0 +1,86 @@
+import random
+import os
+from quotes_kelompok3 import quotesAgama, quotesPendidikan, quotesMotivasi, quotesPercintaan, quotesPolitik
+
+aktif : bool = True
+
+daftarOpsi = [
+    "agama",
+    "pendidikan",
+    "motivasi",
+    "percintaan",
+    "politik",
+    "random"
+]
+
+def QuotesGenerator():
+    while (aktif):
+        Menu()
+
+def Menu():
+    os.system('cls')
+    print("====== Quotes QuotesGenerator =======")
+    print("Jenis quotes")
+    print("1. Agama")
+    print("2. Pendidikan")
+    print("3. Motivasi")
+    print("4. Percintaan")
+    print("5. Politik")
+    print("6. Random")
+
+    jenis = str(input("Pilih jenis Quotes yang anda inginkan (nama): ")).lower()
+
+    Opsi(jenis)
+
+def Opsi(jenis : str):
+    if (jenis not in daftarOpsi):
+        input("Harap pilih salah satu opsi di atas! Tekan Enter untuk mengulang")
+    else:
+        jumlah = int(input("Berapa jumlah quotes yang anda inginkan hari ini [1-5]?: "))
+
+        if (jumlah > 5 or jumlah < 1):
+            input("Harap masukkan batas jumlah yang sesuai! Tekan enter untuk mengulang")
+        else:
+            Quotes(jenis, jumlah)
+
+def Konfirmasi():
+    global aktif
+    while (True):
+        opsi = str(input("Apakah ingin melanjutkan program? [ya/tidak]: "))
+
+        if (opsi == "ya" or opsi == "y"):
+            return True
+        elif (opsi == "tidak" or opsi == "t"):
+            aktif = False
+            return False
+        else:
+            print("Harap masukkan opsi yang benar!")
+
+def Quotes(opsi, jumlah):
+    match(opsi):
+        case "agama":
+            listQuotes = quotesAgama
+        case "pendidikan":
+            listQuotes = quotesPendidikan
+        case "motivasi":
+            listQuotes = quotesMotivasi
+        case "percintaan":
+            listQuotes = quotesPercintaan
+        case "politik":
+            listQuotes = quotesPolitik
+        case "random":
+            listQuotes = [*quotesAgama, *quotesPendidikan, *quotesMotivasi, *quotesPercintaan, *quotesPolitik]
+
+    quotes = random.sample(listQuotes, k=jumlah)
+
+    for i in range(jumlah):
+        print(f"Quote ke-{i + 1}:")
+        quote = quotes[i]
+
+        teksQuote = quote["teks"]
+        tokohQuote = quote["tokoh"]
+        print(f'"{teksQuote}"\n~ {tokohQuote}\n')
+
+    Konfirmasi()
+
+QuotesGenerator()
---
+++ Codes/quotes_kelompok3.py
@@ -0,0 +1,74 @@
+quotesAgama = [
+    {
+        "teks": "Salam Sejahtera",
+        "tokoh": "Budi"
+    },
+    {
+        "teks": "Hidup dengan Sejahtera",
+        "tokoh": "Siti"
+    },
+    {
+        "teks": "Tiada tuhan selain Allah",
+        "tokoh": "Adam"
+    }
+]
+
+quotesPendidikan = [
+    {
+        "teks": "Pendidikan adalah modal hidup",
+        "tokoh": "Seseorang"
+    },
+    {
+        "teks": "Buku adalah jendela dunia",
+        "tokoh": "Dewantara"
+    },
+    {
+        "teks": "1 tambah 1 sama dengan 2",
+        "tokoh": "YTTA"
+    }
+]
+
+quotesMotivasi = [
+    {
+        "teks": "Aku adalah Crypto",
+        "tokoh": "Timo"
+    },
+    {
+        "teks": "Sekolah adalah skem",
+        "tokoh": "Ronald"
+    },
+    {
+        "teks": "Tetap hiduplah meskipun haha hihi",
+        "tokoh": "Aang"
+    }
+]
+
+quotesPercintaan = [
+    {
+        "teks": "Cintailah dirimu sebelum mencintai orang",
+        "tokoh": "Apalah"
+    },
+    {
+        "teks": "Cinta itu buta, tapi uang membutakan",
+        "tokoh": "Poli tikus"
+    },
+    {
+        "teks": "Hidup blonde",
+        "tokoh": "a Gus"
+    }
+]
+
+quotesPolitik = [
+    {
+        "teks": "Pejabat adalah cerminan rakyat",
+        "tokoh": "Au ah"
+    },
+    {
+        "teks": "Wi wok de tok not onli tok de tok",
+        "tokoh": "Mr J"
+    },
+    {
+        "teks": "Hidup jok motor",
+        "tokoh": "Gemoy"
+    }
+]
